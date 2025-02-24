bc3d91885573640c5e0f61c5905e369e613f0f91
limaarthur <artglima.dev@gmail.com>
2025-02-23 23:17:55 -0300

    feat: connecting to api - connecting to axios client api

--- src/components/Orders/ListOrders/index.tsx
+++ src/components/Orders/ListOrders/index.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../Header";
 import { StepsHeader } from "../StepsHeader";
 import { ProductList } from "../ProductList";
+import type { Product } from "../@types/types";
+import { fetchProducts } from "../../../api";
 
 export function ListOrders() {
+  const [products, setProducts] = useState<Product[]>([])
+  console.log(products)
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
     <div>
       <Header />
