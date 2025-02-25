80c7b202d7420620ed91691fbcd8c3504a6acbc1
limaarthur <artglima.dev@gmail.com>
2025-02-25 10:56:35 -0300

    feat: pages and routes - address search with MapTiler and configuring Select

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 const API_URL = 'http://localhost:8080'
+const maptilerToken = 'seu token'
 
 export function fetchProducts() {
   return axios (`${API_URL}/products`)
+}
+
+export function fetchLocalMapTiler(local: string) {
+  return axios(`https://api.maptiler.com/geocoding/${local}.json?key=${maptilerToken}`)
 }
--- src/components/Orders/OrdersLocation/index.tsx
+++ src/components/Orders/OrdersLocation/index.tsx
@@ -1,13 +1,58 @@
+import { useState } from 'react';
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+import AsyncSelect from 'react-select/async';
+
+import { fetchLocalMapTiler } from '../../../api';
 
 import styles from './OrdersLocation.module.css'
 
-const position = {
+const initialPosition = {
   lat: -7.2347669,
   lng: -35.8691761
 }
 
+type Place = {
+  label?: string
+  value?: string
+  position: {
+    lat: number
+    lng: number
+  }
+}
+
 export function OrdersLocation() {
+  const [address, setAddress] = useState<Place>({
+    position: initialPosition
+  })
+
+  const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
+    const response = await fetchLocalMapTiler(inputValue);
+  
+    const places = response.data.features.map((item: any) => {
+      return ({
+        label: item.place_name,
+        value: item.place_name,
+        position: {
+          lat: item.center[1],
+          lng: item.center[0]
+        }
+      });
+    });
+  
+    callback(places);
+  };
+  
+  const handleChangeSelect = (place: Place) => {
+    setAddress(place);
+    //onChangeLocation({
+      //latitude: place.position.lat,
+      //longitude: place.position.lng,
+      //address: place.label!
+    //});
+  };
+
   return (
     <div className={styles.orderLocationContainer}>
       <div className={styles.orderLocationContent}>
@@ -15,21 +60,29 @@ export function OrdersLocation() {
           Selecione onde o pedido deve ser entregue:
         </h3>
         <div className={styles.filterContainer}>
+          <AsyncSelect
+            placeholder="Digite um endereço para entrega do pedido"
+            className={styles.filter}
+            loadOptions={loadOptions}
+            onChange={value => handleChangeSelect(value as Place)}
+          />
         </div>
 
         <MapContainer 
-          center={position} 
-          zoom={13} 
+          center={address.position} 
+          zoom={15} 
+          key={address.position.lat}
           scrollWheelZoom={true}
           className={styles.mapContainer}
         >
+
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
+          <Marker position={address.position}>
             <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              {address.label}
             </Popup>
           </Marker>
         </MapContainer>
